e164c5aa94f5da2a0993d1394636e29ecf20cdaa
DouglasDainese <douglas_dainese@hotmail.com>
2023-09-08 00:22:38 -0300

    feat: cria big-card e small-card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MenuTitleComponent } from './components/menu-title/menu-title.component'
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+import { HomeComponent } from './pages/home/home.component'
+import { BigCardComponent } from './components/big-card/big-card.component'
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    MenuBarComponent,
     MenuTitleComponent,
+    BigCardComponent,
+    SmallCardComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
